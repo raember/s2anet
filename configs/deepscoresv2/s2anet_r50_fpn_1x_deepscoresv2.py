6133153ea6c8fff0f67a268c7e3b0bebc0097e44
raember <raember@users.noreply.github.com>
2021-02-18 13:38:08 +0100

    Adapted the learning rate according to the dota config

--- configs/deepscoresv2/s2anet_r50_fpn_1x_deepscoresv2.py
+++ configs/deepscoresv2/s2anet_r50_fpn_1x_deepscoresv2.py
@@ -139,7 +139,7 @@ data = dict(
 #     gt_dir='data/dota/test/labelTxt/', # change it to valset for offline validation
 #     imagesetfile='data/dota/test/test.txt')
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
