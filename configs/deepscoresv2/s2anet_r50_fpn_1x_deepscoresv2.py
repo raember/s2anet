31b4f0db6cf5091163c59bc93ab725402ae1d403
raember <raember@users.noreply.github.com>
2021-02-23 12:27:33 +0100

    Prepared the adjustment for the training pipeline. WIP

--- configs/deepscoresv2/s2anet_r50_fpn_1x_deepscoresv2.py
+++ configs/deepscoresv2/s2anet_r50_fpn_1x_deepscoresv2.py
@@ -92,8 +92,9 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
+    # dict(type='RandomCrop', crop_size=(1400, 1400)),
     dict(type='RotatedResize', img_scale=(1024, 1024), keep_ratio=True),
-    dict(type='RotatedRandomFlip', flip_ratio=0.5),
+    dict(type='RotatedRandomFlip', flip_ratio=0.0),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
