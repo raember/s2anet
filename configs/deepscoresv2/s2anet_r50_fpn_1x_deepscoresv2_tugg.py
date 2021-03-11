a41f423c870f9728b823942c2a169bc1a96545b6
Lukas Tuggener <tugg@zhaw.ch>
2021-03-11 16:52:00 +0100

    update configs

--- configs/deepscoresv2/s2anet_r50_fpn_1x_deepscoresv2_tugg.py
+++ configs/deepscoresv2/s2anet_r50_fpn_1x_deepscoresv2_tugg.py
@@ -157,7 +157,7 @@ lr_config = dict(
     step=[8, 11])
 checkpoint_config = dict(interval=1)
 log_config = dict(
-    interval=100,
+    interval=1,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='WandbVisualLoggerHook'),
