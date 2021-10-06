7660df2395c2c7116b6937025239d523ce2655bd
Urs Gut <urs_gut@gmx.ch>
2021-10-06 16:35:36 +0200

    Started building a new BatchEnsemble-version of the s2anet-head

--- mmdet/models/anchor_heads_rotated/__init__.py
+++ mmdet/models/anchor_heads_rotated/__init__.py
@@ -2,6 +2,7 @@ from .anchor_head_rotated import AnchorHeadRotated
 from .cascade_s2anet_head import CascadeS2ANetHead
 from .retina_head_rotated import RetinaHeadRotated
 from .s2anet_head import S2ANetHead
+from .s2anet_head_BE import S2ANetHeadBE
 
 __all__ = [
     'AnchorHeadRotated', 'RetinaHeadRotated', 'S2ANetHead', 'CascadeS2ANetHead'
