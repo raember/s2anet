ad2f3fa72af84c2e09b62f6e87c59660ac590f10
raember <raember@users.noreply.github.com>
2021-12-10 12:39:07 +0100

    Moved paths to settings.sh

--- docker/run.sh
+++ docker/run.sh
@@ -3,9 +3,6 @@
 
 . "$(dirname $0)/settings.sh"
 
-CODEBASE="$HOME/s2anet/s2anet"
-DATASET="$HOME/ds2_dense/"
-
 echo "$DOCKER_CMD" run -ditv "$CODEBASE":/s2anet -v "$DATASET":/s2anet/data --name "${1:-$NAME}" "${2:-$TAG}"
 read -p "[ENTER] to run" _
 "$DOCKER_CMD" run -ditv "$CODEBASE":/s2anet -v "$DATASET":/s2anet/data --name "${1:-$NAME}" "${2:-$TAG}"
--- docker/settings.sh
+++ docker/settings.sh
@@ -3,3 +3,5 @@
 DOCKER_CMD='docker'
 TAG='s2anet'
 NAME='s2anet'
+CODEBASE="$HOME/s2anet/s2anet"
+DATASET="$HOME/ds2_dense/"
