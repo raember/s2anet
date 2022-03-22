f9b218df6fa52b76ac0ec15bc7799070ef856efc
Pascal Sager <sage@zhaw.ch>
2022-03-22 22:00:56 +0100

    add argparse

--- DeepScoresV2_s2anet/analyze_errors.py
+++ DeepScoresV2_s2anet/analyze_errors.py
@@ -1,12 +1,39 @@
+import argparse
 import os
 import pickle
-import pandas as pa
+
 import numpy as np
+import pandas as pa
+
+parser = argparse.ArgumentParser(description='Analyze errors')
+# parser.add_argument(
+#         '--ev_folder',
+#         type=str,
+#         default="work_dirs/s2anet_r50_fpn_1x_deepscoresv2_sage_halfrez_crop/overlap/",
+#         help="Path to the folder to evaluate")
+# parser.add_argument(
+#         '--filename',
+#         type=str,
+#         default=".pkl",
+#         help="Name of the file(s) to evalute (must be inside the folder defined by --ev_folder)")
+
+parser.add_argument(
+    '--ev_folder',
+    type=str,
+    default="work_dirs/s2anet_r50_fpn_1x_deepscoresv2_sage_halfrez_crop/",
+    help="Path to the folder to evaluate")
+parser.add_argument(
+    '--filename',
+    type=str,
+    default="dsv2_metrics.pkl",
+    help="Name of the file(s) to evalute (must be inside the folder defined by --ev_folder)")
+args = parser.parse_args()
+
 
 def get_pickles(evaluations_folder):
     error_metrics = dict()
     for base_i, folders_i, files_i in os.walk(evaluations_folder):
-        pickles = [x for x in files_i if "dsv2_metrics.pkl" in x]
+        pickles = [x for x in files_i if args.filename in x]
         if len(pickles) == 1:
             f = open(os.path.join(base_i, pickles[0]), "rb")
             metrics = pickle.load(f)
@@ -18,9 +45,7 @@ def get_pickles(evaluations_folder):
     return error_metrics
 
 
-
 def create_dframe(error_metrics):
-
     dframes = dict()
     for name, values in error_metrics.items():
         row_names = list(values.keys())
@@ -46,26 +71,27 @@ def add_averages(dframes):
     for key, dframe in dframes.items():
         overall_mean = dframe.mean()
         try:
-            variable_mean = dframe.loc[['slur', 'beam', 'tie','dynamicCrescendoHairpin', 'dynamicDiminuendoHairpin'],:].mean()
+            variable_mean = dframe.loc[['slur', 'beam', 'tie', 'dynamicCrescendoHairpin', 'dynamicDiminuendoHairpin'],
+                            :].mean()
         except:
             variable_mean = overall_mean * 0
-        dframe = dframe.append([overall_mean,variable_mean])
+        dframe = dframe.append([overall_mean, variable_mean])
         dframe = dframe.rename(index={0: "overall_mean", 1: "variable_mean"})
         dframes[key] = dframe
 
     return dframes
 
+
 def store_csv(dframes, evaluations_folder):
     for key, dframe in dframes.items():
-        path = os.path.join(evaluations_folder, key+"_metrics.csv")
+        path = os.path.join(evaluations_folder, key + "_metrics.csv")
         print(path)
         dframe.to_csv(path)
     return None
 
 
 def main():
-
-    evaluations_folder = "/s2anet/work_dirs/s2anet_r50_fpn_1x_deepscoresv2_sage_halfrez_crop/result_4"
+    evaluations_folder = args.ev_folder
     error_metrics = get_pickles(evaluations_folder)
     dframes = create_dframe(error_metrics)
 
