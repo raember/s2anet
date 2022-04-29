e6d95b97a8bb062e005b25376169b687ce0c1fd2
raember <raember@users.noreply.github.com>
2022-04-29 11:49:06 +0200

    Updated inference script

--- tools/inference_ismlp.py
+++ tools/inference_ismlp.py
@@ -1,10 +1,9 @@
+from pathlib import Path
 from mmdet.apis import init_detector, inference_detector
-import mmcv
 import os
 import cv2
 from mmdet.core import rotated_box_to_poly_np
 from mmcv.visualization import imshow_det_bboxes
-import numpy as np
 from obb_anns import OBBAnns
 
 annotations_file = "data/deep_scores_dense/deepscores_test.json"
@@ -13,26 +12,23 @@ obb.load_annotations()
 obb.set_annotation_set_filter(['deepscores'])
 CLASSES = tuple([v["name"] for (k, v) in obb.get_cats().items()])
 
-config_file = 'configs/deepscoresv2/s2anet_r50_fpn_1x_deepscoresv2_tugg_lowrez.py'
-checkpoint_file = 'work_dirs/s2anet_r50_fpn_1x_deepscoresv2_tugg_lowrez/epoch_500.pth'
+config_file = 'configs/deepscoresv2/s2anet_r50_fpn_1x_deepscoresv2_tugg_halfrez_crop.py'
+checkpoint_file = 'checkpoint.pth'
 
 model_name = "s2anet_fullrez_crop"
-images_folder = "warped"
-out_folder = "warped_out"
-
-resize = 1.3
+images_folder = Path("input")
+out_folder = Path("input_out")
+images_folder.mkdir(exist_ok=True)
+out_folder.mkdir(exist_ok=True)
 
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
 
-images = os.listdir(images_folder)
-os.makedirs(os.path.join(images_folder, model_name), exist_ok=True)
-
-for img in images:
-    if os.path.isdir(os.path.join(images_folder, img)):
+for file in images_folder.glob('*'):
+    if not file.is_file():
         continue
-    print(f"Processing {img}")
-    img_loaded = cv2.imread(os.path.join(images_folder, img))
-    img_loaded = cv2.resize(img_loaded, (int(img_loaded.shape[1]*resize), int(img_loaded.shape[0]*resize)), interpolation = cv2.INTER_AREA)
+    print(f"Processing {file.name}")
+    img_loaded = cv2.imread(str(file))
+    # img_loaded = cv2.resize(img_loaded, (int(img_loaded.shape[1]*resize), int(img_loaded.shape[0]*resize)), interpolation = cv2.INTER_AREA)
     result = inference_detector(model, img_loaded)
 
     det_boxes = rotated_box_to_poly_np(result[1][0][0].cpu().numpy())
@@ -41,5 +37,5 @@ for img in images:
     img_det = imshow_det_bboxes(img_loaded, det_boxes,
                                 det_labels.astype(int) + 1,
                                 class_names=CLASSES, show=False, show_label=True, rotated=True)
-
-    cv2.imwrite(os.path.join(out_folder, img), img_det)
+    cv2.imwrite(out_folder/ file.name, img_det)
+print('Done')
