a4a9c53fe7bc6808cc6084cd9ba1faa7025f308c
raember <raember@users.noreply.github.com>
2022-05-04 16:28:07 +0200

    Fixed dataset modification script to rescale new samples

--- tools/ds_dataset.py
+++ tools/ds_dataset.py
@@ -1,7 +1,8 @@
 import csv
 import math
+from pathlib import Path
 
-import pandas
+import numpy
 from PIL import Image
 
 from mmdet.core import rotated_box_to_poly_single
@@ -10,9 +11,10 @@ from obb_anns import OBBAnns
 MAIN_ANN_FILE = 'data/deep_scores_dense/imslp_test.json'
 DELETE_ENTRIES = False
 NEW_ANNS = {
-    'Liszt_Franz_Konzertetude.png': 'Liszt_Franz_Konzertetude_Annotations.csv'
+    'in/Liszt_Franz_Konzertetude.png': 'in/Liszt_Franz_Konzertetude_Annotations.csv',
 }
-ANN_OUT_FILE = 'data/deep_scores_dense/imslp_test_new.json'
+ANN_OUT_FILE = Path('data/deep_scores_dense/imslp_test_new.json')
+WIDTH = 1960.0
 
 if __name__ == '__main__':
     anns = OBBAnns(MAIN_ANN_FILE)
@@ -26,14 +28,16 @@ if __name__ == '__main__':
     anns_id = anns.ann_info.shape[0]
     for img_file, ann_file in NEW_ANNS.items():
         img = Image.open(img_file)
-        img = img.resize((1960, int(1960.0 / img.width * img.height)), Image.NEAREST)
+        scale = WIDTH / img.width
+        img = img.resize((int(WIDTH), int(scale * img.height)), Image.NEAREST)
         with open(ann_file, 'r') as csvfile:
             new_anns = {}
             next_img_id = max(anns.img_idx_lookup.keys()) + 1
             for row in csv.reader(csvfile):
                 cls_name = row[0]
-                rects = list(map(int, row[1:]))
-                rects[-1] = rects[-1] / 180.0 * math.pi
+                rects = [int(scale * float(val)) for val in row[1:5]]
+                angle = float(row[5])
+                rects.append(angle / 180.0 * math.pi)
                 o_bbox = rotated_box_to_poly_single(rects)
                 a_bbox = list(map(int, [max(o_bbox[0::2]), min(o_bbox[1::2]), min(o_bbox[0::2]), max(o_bbox[1::2])]))
                 area = rects[2] * rects[3]
@@ -41,12 +45,12 @@ if __name__ == '__main__':
                 anns_id += 1
                 new_anns[anns_id] = {
                     'a_bbox': a_bbox,
-                    'o_bbox': o_bbox.tolist(),
+                    'o_bbox': o_bbox.astype(numpy.int).tolist(),
                     'cat_id': [str(cat_id)],
                     'area': area,
                     'img_id': next_img_id,
                     'comments': ''
                 }
-            anns.add_new_img_ann_pair(img_file, img.width, img.height, new_anns)
-
+        anns.add_new_img_ann_pair(img_file, img.width, img.height, new_anns)
+        img.save(ANN_OUT_FILE.parent / 'images' / Path(img_file).name)
     anns.save_annotations(ANN_OUT_FILE)
