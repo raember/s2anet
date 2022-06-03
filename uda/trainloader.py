8060d5eb872a31d42521c3ec287a99d82eeea130
Adhiraj Ghosh <adhirajghosh1998@gmail.com>
2022-06-03 10:53:39 +0200

    Delete trainloader.py

--- uda/trainloader.py
+++
@@ -1,9 +0,0 @@
-import _pickle as pickle
-
-a_file = open("uda/ds2.pkl", "rb")
-unpickler = pickle.Unpickler(a_file)
-# if file is not empty scores will be equal
-# to the value unpickled
-scores = unpickler.load()
-print("Done")
-#output = pickle.load(a_file)
