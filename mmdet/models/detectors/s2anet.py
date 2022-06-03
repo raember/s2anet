371bf287e022c2d5ebdfe68fcdfe03f1b1198c18
Adhiraj Ghosh <adhirajghosh1998@gmail.com>
2022-06-03 15:32:57 +0200

    Merge branch 'dev-ghos' into dev

--- mmdet/models/detectors/s2anet.py
+++ mmdet/models/detectors/s2anet.py
@@ -1,4 +1,4 @@
-from .single_stage import SingleStageDetector
+from .single_stage import SingleStageDetector, SingleStageDetectorDA
 from ..registry import DETECTORS
 
 
@@ -13,4 +13,18 @@ class S2ANetDetector(SingleStageDetector):
                  test_cfg=None,
                  pretrained=None):
         super(S2ANetDetector, self).__init__(backbone, neck, bbox_head, train_cfg,
+                                             test_cfg, pretrained)
+
+
+@DETECTORS.register_module
+class S2ANetUDA(SingleStageDetectorDA):
+
+    def __init__(self,
+                 backbone,
+                 neck,
+                 bbox_head,
+                 train_cfg=None,
+                 test_cfg=None,
+                 pretrained=None):
+        super(S2ANetUDA, self).__init__(backbone, neck, bbox_head, train_cfg,
                                              test_cfg, pretrained)
--- mmdet/models/detectors/ssd_uda.py
+++ mmdet/models/detectors/ssd_uda.py
@@ -7,9 +7,8 @@ from .base import BaseDetector
 
 
 @DETECTORS.register_module
-class SingleStageDetectorUDA(BaseDetector):
+class SingleStageDetectorDA(BaseDetector):
     """Base class for single-stage detectors.
-
     Single-stage detectors directly and densely predict bounding boxes on the
     output features of the backbone+neck.
     """
@@ -21,10 +20,8 @@ class SingleStageDetectorUDA(BaseDetector):
                  train_cfg=None,
                  test_cfg=None,
                  pretrained=None):
-        super(SingleStageDetectorUDA, self).__init__()
-        self.backbone1 = builder.build_backbone(backbone)
-        self.backbone2 = builder.build_backbone(backbone)
-
+        super(SingleStageDetector, self).__init__()
+        self.backbone = builder.build_backbone(backbone)
         if neck is not None:
             self.neck = builder.build_neck(neck)
         self.bbox_head = builder.build_head(bbox_head)
@@ -34,8 +31,8 @@ class SingleStageDetectorUDA(BaseDetector):
         self.last_vals = None
 
     def init_weights(self, pretrained=None):
-        super(SingleStageDetectorUDA, self).init_weights(pretrained)
-        self.backbone1.init_weights(pretrained=pretrained)
+        super(SingleStageDetectorDA, self).init_weights(pretrained)
+        self.backbone.init_weights(pretrained=pretrained)
         if self.with_neck:
             if isinstance(self.neck, nn.Sequential):
                 for m in self.neck:
@@ -54,7 +51,6 @@ class SingleStageDetectorUDA(BaseDetector):
 
     def forward_dummy(self, img):
         """Used for computing network flops.
-
         See `mmedetection/tools/get_flops.py`
         """
         x = self.extract_feat(img)
@@ -62,28 +58,19 @@ class SingleStageDetectorUDA(BaseDetector):
         return outs
 
     def forward_train(self,
-                      src_img,
-                      tgt_img,
+                      img,
                       img_metas,
                       gt_bboxes,
                       gt_labels,
                       gt_bboxes_ignore=None):
-
-        #Source
-        x_src = self.extract_feat(src_img)
-        outs_src = self.bbox_head(x_src)
-        loss_inputs = outs_src + (gt_bboxes, gt_labels, img_metas, self.train_cfg)
+        x = self.extract_feat(img)
+        outs = self.bbox_head(x)
+        loss_inputs = outs + (gt_bboxes, gt_labels, img_metas, self.train_cfg)
         losses = self.bbox_head.loss(
             *loss_inputs, gt_bboxes_ignore=gt_bboxes_ignore)
 
-        self.last_vals = dict(img=src_img)
-
-        #Target
-        x_tgt = self.extract_feat(tgt_img)
-        outs_tgt = self.bbox_head(x_tgt) #DO WE NEED THIS?
-        return losses, x_src, x_tgt
-        # return losses, outs_src, outs_tgt
-
+        self.last_vals = dict(img=img)
+        return losses
 
     def simple_test(self, img, img_meta, rescale=False):
         x = self.extract_feat(img)
