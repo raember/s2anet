b9e34219b2034f40418cdf40ef6b4f6bcd6195d9
Adhiraj Ghosh <adhirajghosh1998@gmail.com>
2022-06-03 10:51:22 +0200

    Train for uda: final

--- uda/train.py
+++ uda/train.py
@@ -1,3 +1,5 @@
+#HERE WE TAKE FEATURES EXTRACTOR FROM S2ANET DIRECTLY
+
 from __future__ import division
 
 import argparse
@@ -5,14 +7,13 @@ import os
 import os.path as osp
 import warnings
 import wandb
-import torch
-import torch.nn.functional as F
 from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import LambdaLR
 from torchvision import transforms, utils
 from mmcv import Config
-from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
-from mmdet.apis import (get_root_logger, init_dist, set_random_seed,
-                        train_detector)
+from mmcv.parallel import MMDistributedDataParallel
+
+from mmdet.apis import set_random_seed
 from mmdet.apis.train import build_optimizer
 from mmdet.datasets import build_dataset, build_dataloader
 from mmdet.models import build_detector
@@ -20,15 +21,17 @@ from imslp import ImslpDataset
 from transforms_imslp import *
 from core.utils.metric_logger import MetricLogger
 from core.utils.logger import setup_logger
-from core.models import build_adversarial_discriminator, build_feature_extractor, build_classifier
-from core.solver import adjust_learning_rate
-from core.utils.loss_ops import reduce_loss_dict, summarise_loss, soft_label_cross_entropy
+from core.utils.gl import WarmStartGradientLayer
+from core.models import build_binary_discriminator
+from core.solver import DomainAdversarialLoss
+from core.utils.loss_ops import reduce_loss_dict, summarise_loss
 warnings.filterwarnings("ignore")
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
-    parser.add_argument('--config', help='test config file path', default = 'configs/deepscoresv2/s2anet_r50_fpn_1x_deepscoresv2_tugg_halfrez_crop.py')
-    parser.add_argument('--work_dir', help='the dir to save logs and models', default='models/uda/no_pretrain/')
+    # parser.add_argument('--config', help='test config file path', default = 'configs/deepscoresv2/s2anet_r50_fpn_1x_deepscoresv2_sage_halfrez_crop.py')
+    parser.add_argument('--config', help='test config file path', default = 'configs/deepscoresv2/ghos_uda.py')
+    parser.add_argument('--work_dir', help='the dir to save logs and models', default='models/uda_test_model_june/pretrained')
     parser.add_argument(
         '--resume_from', help='the checkpoint file to resume from', default = 'models/deepscoresV2_tugg_halfrez_crop_epoch250.pth')
         # '--resume_from', help='the checkpoint file to resume from', default = None)
@@ -39,7 +42,7 @@ def parse_args():
     parser.add_argument(
         '--gpus',
         type=int,
-        default=3,
+        default=2,
         help='number of gpus to use '
              '(only applicable to non-distributed training)')
     parser.add_argument('--seed', type=int, default=None, help='random seed')
@@ -60,7 +63,7 @@ def parse_args():
     return args
 
 def main():
-    wandb.init(project="uda", entity="adhirajghosh")
+    # wandb.init(project="omr-uda", entity="adhirajghosh")
     args = parse_args()
 
     cfg = Config.fromfile(args.config)
@@ -72,15 +75,15 @@ def main():
     elif cfg.get('work_dir', None) is None:
         cfg.work_dir = osp.join('./work_dirs',
                                 osp.splitext(osp.basename(args.config))[0])
-    if args.resume_from is not None:
-        cfg.resume_from = args.resume_from
+    if not cfg.resume:
+        args.resume_from = None
     cfg.gpus = args.gpus
 
     if args.autoscale_lr:
         cfg.optimizer['lr'] = cfg.optimizer['lr'] * cfg.gpus / 8
 
-    num_gpus = 3
-    distributed = True
+    num_gpus = cfg.num_gpu
+    distributed = cfg.distributed
     args.local_rank = 1
 
     if distributed:
@@ -88,6 +91,7 @@ def main():
 
     logger = setup_logger('UDA', args.work_dir, args.local_rank)
     logger.info("Using {} GPUs".format(num_gpus))
+    logger.info("Pretrained: {}".format(cfg.resume))
     logger.info(args)
     logger.info('Distributed training: {}'.format(distributed))
 
@@ -95,86 +99,69 @@ def main():
         logger.info('Set random seed to {}'.format(args.seed))
         set_random_seed(args.seed)
 
+    #S2ANet and IMSLP Dataset Loading Happens Here
     datasets = [build_dataset(cfg.data.train)]
     if len(cfg.workflow) == 2:
         datasets.append(build_dataset(cfg.data.val))
     datasets = datasets if isinstance(datasets, (list, tuple)) else [datasets]
     dataloader_src = [
         build_dataloader(
-            ds, cfg.data.imgs_per_gpu, cfg.data.workers_per_gpu, dist=True)
+            ds, cfg.data.imgs_per_gpu, cfg.data.workers_per_gpu, num_gpus = num_gpus, dist=True)
         for ds in datasets
     ]
 
     data_transform = transforms.Compose([ToTensor()])
-    imslp_dataset = ImslpDataset(split_file='./data/imslp_dataset/train_test_split/train_list.txt',
-                                 root_dir='./data//imslp_dataset/images/', transform=data_transform)
-    dataloader_tgt = DataLoader(imslp_dataset,batch_size=2, shuffle=True,num_workers=2)
+    imslp_dataset = ImslpDataset(split_file
+                                 ='data/imslp_dataset/train_test_split/train_list_no_landscape.txt',
+                                 root_dir='data/imslp_dataset/images/', transform=data_transform)
+    dataloader_tgt = DataLoader(imslp_dataset,batch_size=4, shuffle=True,num_workers=2)
 
-    # put model on gpus
+    # Model Loading Happens Here
     s2anet = build_detector(
         cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
-    s2anet = MMDistributedDataParallel(s2anet.cuda())
+
     s2anet.CLASSES = datasets[0].CLASSES
 
-
-    cfg2 = Config.fromfile('./uda/configs/r50_adv_ghos.yaml')
-    feature_extractor = build_feature_extractor(cfg2)
-    feature_extractor.cuda()
-
-    model_D = build_adversarial_discriminator(cfg2)
+    s2anet_feat = 128
+    s2anet_hidden = 256
+    model_D = build_binary_discriminator(num_gpus, s2anet_feat, s2anet_hidden)
     model_D.cuda()
 
-    classifier = build_classifier(cfg2)
-    classifier.cuda()
-    dev_ids = [0,1,2]
     if distributed:
-        pg1 = torch.distributed.new_group(range(torch.distributed.get_world_size()))
-        feature_extractor = torch.nn.parallel.DistributedDataParallel(
-                feature_extractor, device_ids=dev_ids, output_device=2,
-            find_unused_parameters=False, process_group=pg1
-        )
+        s2anet = MMDistributedDataParallel(s2anet.cuda())
 
-        pg2 = torch.distributed.new_group(range(torch.distributed.get_world_size()))
-        classifier = torch.nn.parallel.DistributedDataParallel(
-            classifier, device_ids=dev_ids, output_device=2,
-            find_unused_parameters=False, process_group=pg2
-        )
-        pg3 = torch.distributed.new_group(range(torch.distributed.get_world_size()))
-        model_D = torch.nn.parallel.DistributedDataParallel(
-            model_D, device_ids=dev_ids, output_device=2,
-            find_unused_parameters=False, process_group=pg3
-        )
         torch.autograd.set_detect_anomaly(True)
         torch.distributed.barrier()
 
+    #Optimizers are Set Here
     optimizer_s2anet = build_optimizer(s2anet, cfg.optimizer)
     optimizer_s2anet.zero_grad()
 
-    optimizer_fea = torch.optim.SGD(feature_extractor.parameters(), lr=cfg2.SOLVER.BASE_LR,
-                                    momentum=cfg2.SOLVER.MOMENTUM,
-                                    weight_decay=cfg2.SOLVER.WEIGHT_DECAY)
-    optimizer_fea.zero_grad()
-
-    optimizer_D = torch.optim.Adam(model_D.parameters(), lr=cfg2.SOLVER.BASE_LR_D, betas=(0.9, 0.99))
+    optimizer_D = torch.optim.Adam(model_D.parameters(), lr=cfg.optimizer_d['lr'], betas=(0.9, 0.99))
     optimizer_D.zero_grad()
 
-    optimizer_cls = torch.optim.SGD(classifier.parameters(), lr=cfg2.SOLVER.BASE_LR*10, momentum=cfg2.SOLVER.MOMENTUM, weight_decay=cfg2.SOLVER.WEIGHT_DECAY)
-    optimizer_cls.zero_grad()
+    #Schedulers are Set Here
+    lr_scheduler_s2anet = LambdaLR(optimizer_s2anet, lambda x: cfg.optimizer['lr'] * (1. + cfg.optimizer_d['gamma'] * float(x)) ** (-cfg.optimizer['weight_decay']))
+    lr_scheduler_d = LambdaLR(optimizer_D, lambda x: cfg.optimizer_d['lr'] * (1. + cfg.optimizer_d['gamma'] * float(x)) ** (-cfg.optimizer_d['weight_decay']))
 
-    logger.info(feature_extractor)
+    # define loss function
+    domain_adv = DomainAdversarialLoss().cuda()
+    gl = WarmStartGradientLayer(alpha=1., lo=0., hi=1., max_iters=1000, auto_step=True)
+
+    # Load from checkpoint
+    if cfg.resume:
+        checkpoint = torch.load(args.resume_from)
+        s2anet.load_state_dict(checkpoint['state_dict'])
+        optimizer_s2anet.load_state_dict(checkpoint['optimizer'])
+
     logger.info(model_D)
-    logger.info(classifier)
 
-    s2anet.train()
-    feature_extractor.train()
-    model_D.train()
-    classifier.train()
-
-    max_iters = cfg2.SOLVER.MAX_ITER
     meters = MetricLogger(delimiter="  ")
     logger.info("Start training")
     for epoch in range(num_epochs):
         print("For epoch ", epoch+1)
+        print("lr S2ANet:", lr_scheduler_s2anet.get_lr())
+        print("lr discriminator:", lr_scheduler_d.get_lr())
         for i_batch, ((src), (tgt)) in enumerate(zip(dataloader_src[0], dataloader_tgt)):
             src_img = src['img']
             src_meta = src['img_meta']
@@ -183,75 +170,65 @@ def main():
 
             tgt_img = tgt['image'].cuda(non_blocking=True).float()
 
-            current_lr = adjust_learning_rate(cfg2.SOLVER.LR_METHOD, cfg2.SOLVER.BASE_LR, epoch, max_iters,
-                                                      power=cfg2.SOLVER.LR_POWER)
-            current_lr_D = adjust_learning_rate(cfg2.SOLVER.LR_METHOD, cfg2.SOLVER.BASE_LR_D, epoch, max_iters,
-                                                power=cfg2.SOLVER.LR_POWER)
-            for index in range(len(optimizer_fea.param_groups)):
-                optimizer_fea.param_groups[index]['lr'] = current_lr
-            for index in range(len(optimizer_s2anet.param_groups)):
-                optimizer_s2anet.param_groups[index]['lr'] = current_lr
-            for index in range(len(optimizer_cls.param_groups)):
-                optimizer_cls.param_groups[index]['lr'] = current_lr * 10
-            for index in range(len(optimizer_D.param_groups)):
-                optimizer_D.param_groups[index]['lr'] = current_lr_D
+            # Step 1: Train S2ANet, freeze the discriminator
+            s2anet.train()
+            model_D.eval()
+            for param in s2anet.parameters():
+                param.requires_grad = True
 
-            optimizer_fea.zero_grad()
-            optimizer_cls.zero_grad()
-            optimizer_D.zero_grad()
+            for param in model_D.parameters():
+                param.requires_grad = False
+            src = True
+            losses, src_fea = s2anet.train_step(src_img, src_meta, src_gtbboxes, src_gtlabels, src)
+            loss_obj, loss_reduced = summarise_loss(losses)
 
-            src_size = src_img.data[0].shape[-2:]
-            tgt_size = tgt_img.data.shape[-2:]
+            src = False
+            tgt_fea = s2anet.train_step(tgt_img, src_meta, src_gtbboxes, src_gtlabels, src)
 
-            outputs = s2anet.train_step(src_img, src_meta, src_gtbboxes, src_gtlabels, optimizer_s2anet)
-            # outputs = s2anet(src_img, src_meta, src_gtbboxes, src_gtlabels)
-            loss_dict_reduced = reduce_loss_dict(outputs)
-            loss_obj, loss_reduced = summarise_loss(loss_dict_reduced)
-            # loss_obj.backward() #happens in train_step()
-            # print(outputs)
-
-            src_fea = feature_extractor(src_img.data[0])
-            # src_fea2 = src_fea.cuda()
-            src_pred = classifier(src_fea, src_size)
-            temperature = 1.8
-            src_pred = src_pred.div(temperature)
-            src_soft_label = F.softmax(src_pred, dim=1).detach()
-            src_soft_label[src_soft_label > 0.9] = 0.9
-
-            tgt_fea = feature_extractor(tgt_img)
-            # tgt_fea2 = tgt_fea.cuda()
-            tgt_pred = classifier(tgt_fea, tgt_size)
-            tgt_pred = tgt_pred.div(temperature)
-            tgt_soft_label = F.softmax(tgt_pred, dim=1)
-            tgt_soft_label = tgt_soft_label.detach()
-            tgt_soft_label[tgt_soft_label > 0.9] = 0.9
-
-            tgt_D_pred = model_D(tgt_fea, tgt_size)
-            loss_adv_tgt = 0.001 * soft_label_cross_entropy(tgt_D_pred, torch.cat(
-                (tgt_soft_label, torch.zeros_like(tgt_soft_label)), dim=1))
-            loss_adv_tgt.backward()
-
-            optimizer_fea.step()
             optimizer_s2anet.step()
-            optimizer_cls.step()
+
+            # adversarial training to fool the discriminator
+            src_fea = torch.stack(list(src_fea[0]), dim=0)
+            tgt_fea = torch.stack(list(tgt_fea[0]), dim=0)
+
+            src_fea_tensor = torch.tensor(src_fea, requires_grad=True)
+            tgt_fea_tensor = torch.tensor(tgt_fea, requires_grad=True)
+
+            feat_concat = torch.cat((src_fea_tensor, tgt_fea_tensor), dim=0).cuda()
+
+            d = model_D(gl(feat_concat))
+            d_s, d_t = d.chunk(2, dim=0)
+
+            loss_transfer = 0.5 * (domain_adv(d_s, 'target') + domain_adv(d_t, 'source'))
+            optimizer_s2anet.zero_grad()
+
+            (loss_transfer+cfg.trade_off).backward()
+            optimizer_s2anet.step()
+            lr_scheduler_s2anet.step()
+
+            # Step 2: Train the discriminator
+            s2anet.eval()
+            model_D.train()
+            for param in s2anet.parameters():
+                param.requires_grad = False
+
+            for param in model_D.parameters():
+                param.requires_grad = True
+
+            d = model_D(feat_concat.detach())
+            d_s, d_t = d.chunk(2, dim=0)
+            loss_discriminator = 0.5 * (domain_adv(d_s, 'source') + domain_adv(d_t, 'target'))
 
             optimizer_D.zero_grad()
-            # torch.distributed.barrier()
-
-            src_D_pred = model_D(src_fea.detach(), src_size)
-            loss_D_src = 0.5 * soft_label_cross_entropy(src_D_pred,torch.cat((src_soft_label, torch.zeros_like(src_soft_label)),dim=1))
-            loss_D_src.backward()
-
-            tgt_D_pred = model_D(tgt_fea.detach(), tgt_size)
-            loss_D_tgt = 0.5 * soft_label_cross_entropy(tgt_D_pred,torch.cat((torch.zeros_like(tgt_soft_label), tgt_soft_label),dim=1))
-            loss_D_tgt.backward()
+            loss_discriminator.backward()
             optimizer_D.step()
+            lr_scheduler_d.step()
+
 
             meters.update(loss_obj=loss_obj.item())
-            meters.update(loss_adv_tgt=loss_adv_tgt.item())
-            meters.update(loss_D=(loss_D_src.item() + loss_D_tgt.item()))
-            meters.update(loss_D_src=loss_D_src.item())
-            meters.update(loss_D_tgt=loss_D_tgt.item())
+            meters.update(loss_transfer=loss_transfer.item())
+            meters.update(loss_discriminator=loss_discriminator.item())
+
 
             if i_batch % 20 == 0:
                 logger.info('Epoch: [{0}][{1}/{2}]\t'
@@ -259,23 +236,23 @@ def main():
                       'lr: {lr:.6f}\t'.format(
                     epoch + 1, i_batch + 1, len(dataloader_src[0]),
                     meters=str(meters),
-                    lr=optimizer_fea.param_groups[0]["lr"],
+                    lr=optimizer_s2anet.param_groups[0]["lr"],
                 ))
+
                 wandb.log({
                     "Detection Loss ": loss_obj.item(),
-                    "Adversarial Loss (Target)": loss_adv_tgt.item(),
-                    "Discriminator Loss (Source)": loss_D_src.item(),
-                    "Discriminator Loss (Target)": loss_D_tgt.item()
+                    "Domain Transfer Loss": loss_transfer.item(),
+                    "Discriminator Loss": loss_discriminator.item(),
                 })
 
+        meta = {}
+        meta['epoch'] = epoch
+        meta['CLASSES'] = datasets[0].CLASSES
         if epoch % 10 == 0 or epoch == num_epochs:
             logger.info("Saving model")
             filename = os.path.join(args.work_dir, "model_{:03d}.pth".format(epoch))
-            torch.save({'epoch': epoch, 'detector': s2anet.state_dict(),
-                        'feature_extractor': feature_extractor.state_dict(),
-                        'classifier': classifier.state_dict(), 'model_D': model_D.state_dict(),
-                        'optimizer_fea': optimizer_fea.state_dict(), 'optimizer_det': optimizer_s2anet.state_dict(),
-                        'optimizer_D': optimizer_D.state_dict()}, filename)
+
+            torch.save({'meta': meta, 'state_dict': s2anet.state_dict(), 'optimizer': optimizer_s2anet.state_dict()}, filename)
 
 
 if __name__ == '__main__':
